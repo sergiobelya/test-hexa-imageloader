30d6e022d7095ac47aaf8978686848e00b46ea49
sergiobelya <sergiobelya@gmail.com>
2017-02-15 18:41:33 +0200

    test composer

---
+++ sergiobelya/TestHexaImageloader/src/testcomposer.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace sergiobelya\TestHexaImageloader;
+
+/**
+ * Description of testcomposer
+ *
+ * @author Serg
+ */
+class TestComposer
+{
+	public static function test()
+	{
+		echo 12345;
+	}
+}
